25e836ec9dfd723c162474375648685bca4cad27
HashedDan <georgedanielmangum@gmail.com>
2018-03-31 09:14:28 -0500

    dynamic post pages

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,21 +1,28 @@
 import React from 'react'
+import Link from "gatsby-link";
 
-const IndexPage = ({ data }) => (
-  <div>
-      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h3>
-            {node.frontmatter.title}{" "}
-            <span color="#BBB">— {node.frontmatter.date}</span>
-          </h3>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
+const PostsPage = ({ data }) => (
+    <div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+                <h3>
+                    <Link
+                        to={node.fields.slug}
+                        css={{ textDecoration: `none`, color: `inherit` }}
+                    >
+                        {node.frontmatter.title}
+                    </Link>
+                    {" "}
+                    <span color="#BBB">— {node.frontmatter.date}</span>
+                </h3>
+                <p>{node.excerpt}</p>
+            </div>
+        ))}
     </div>
 )
 
-export default IndexPage
+export default PostsPage
 
 export const query = graphql`
   query IndexQuery {
@@ -27,6 +34,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
