3d369619b714ebc0a364bef9e480284de42c1781
HashedDan <georgedanielmangum@gmail.com>
2018-03-31 15:49:34 -0500

    menu updates

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton'
+import Toolbar from 'material-ui/ToolBar'
+import Link, { navigateTo } from 'gatsby-link'
+import DropDownMenu from 'material-ui/DropDownMenu';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 
 import Header from '../components/Header'
 
 const TemplateWrapper = ({ data, children }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    {/* <Header data={data} /> */}
-    {/* <AppBar
-    title={data.site.siteMetadata.title}
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  /> */}
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <MuiThemeProvider>
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <AppBar
+        title={data.site.siteMetadata.title}
+        onTitleClick={() => navigateTo('/')}
+        style={{ backgroundColor: '#000080', position: 'fixed' }}
+        showMenuIconButton={false}
+        children={
+          <div style={{ margin: 'auto' }}>
+            <IconMenu
+              iconButtonElement={<IconButton><MenuIcon color="white"/></IconButton>}
+              anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+              targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+              style={{ color: 'white' }}
+            >
+              <MenuItem primaryText="Posts" onClick={() => navigateTo('/posts')} />
+              <MenuItem primaryText="Podcasts" onClick={() => navigateTo('/posts')} />
+            </IconMenu>
+          </div>
+        }
+      />
+
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 90,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
+  </MuiThemeProvider>
 )
 
 TemplateWrapper.propTypes = {
