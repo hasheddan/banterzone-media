bb9b9d47ed3a8d1ec92be9d6321b623ee644bea1
HashedDan <georgedanielmangum@gmail.com>
2018-04-04 16:08:40 -0500

    initial integration of netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
