f83e64b9098d02aa0c73161e415204a9b10a5dec
HashedDan <georgedanielmangum@gmail.com>
2018-04-08 12:25:02 -0500

    still fixing issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/static/posts`,
       },
     },
     // {
